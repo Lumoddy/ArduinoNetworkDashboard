23bdba9c50fe2a623b2ee7f3f48ecfe4b91de0c6
MrJaydanOz <jaydan@jaysanlunt.com.au>
2025-03-30 10:04:12 +1100

    Modified classes NetworkNodes and SerialNodes to support connecting

--- js/network-nodes.js
+++ js/network-nodes.js
@@ -1,4 +1,28 @@
-import { LoudArray, ReadonlyLoudArray } from "./loud";
+import { CustomEventHandler } from "./custom-events.js";
+import { SerialNode, SerialRootNode } from "./serial-nodes.js";
+
+/**
+@typedef {{
+    "serialNodeChanged":
+    {
+        target: NetworkNode,
+        oldSerialNode: SerialNode | null,
+        newSerialNode: SerialNode | null,
+    },
+    "serialNodeConnected":
+    {
+        target: NetworkNode,
+        targetSerialNode: SerialNode,
+        otherSerialNode: SerialNode | SerialRootNode,
+    },
+    "serialNodeDisconnected":
+    {
+        target: NetworkNode,
+        targetSerialNode: SerialNode,
+        otherSerialNode: SerialNode | SerialRootNode,
+    },
+}} NetworkNodeEventMap
+*/
 
 /**
 */ export class NetworkNode
@@ -7,17 +31,140 @@ import { LoudArray, ReadonlyLoudArray } from "./loud";
     @public*/ constructor()
     {
         /**
-        @type {LoudArray<NetworkNode | NetworkRootNode>}
-        @private*/ this._connectedTo = new LoudArray();
+        @type {SerialNode | null}
+        @private*/ this._serialNode = null;
+
+        /**
+        @type {CustomEventHandler<{ [K in keyof NetworkNodeEventMap]: [event: NetworkNodeEventMap[K]] }>}
+        @private*/ this._eventHandler = new CustomEventHandler();
+
+        /**
+        @type {(event: import("./serial-nodes").SerialNodeEventMap["nodeConnected"]) => void}
+        @private*/ this._serialOnNodeConnected = (e) =>
+        {
+            this._eventHandler.dispatch("serialNodeConnected", {
+                target: this,
+                targetSerialNode: e.target,
+                otherSerialNode: e.otherNode,
+            });
+        };
+
+        /**
+        @type {(event: import("./serial-nodes").SerialNodeEventMap["nodeConnected"]) => void}
+        @private*/ this._serialOnNodeDisconnected = (e) =>
+        {
+            this._eventHandler.dispatch("serialNodeDisconnected", {
+                target: this,
+                targetSerialNode: e.target,
+                otherSerialNode: e.otherNode,
+            });
+        };
+    }
+
+    /**
+    @returns {SerialNode | null}
+    @public*/ get serialNode() { return this._serialNode }
+    /**
+    @param {SerialNode | null} node
+    @public*/ setSerialNode(node)
+    {
+        if (this._serialNode !== null)
+        {
+            this._serialNode.addEventListener("nodeConnected", this._serialOnNodeConnected);
+            this._serialNode.addEventListener("nodeDisconnected", this._serialOnNodeDisconnected);
+        }
+
+        const oldNode = this._serialNode;
+        this._serialNode = node;
+
+        this._eventHandler.dispatch("serialNodeChanged", {
+            target: this,
+            oldSerialNode: oldNode,
+            newSerialNode: node,
+        });
     }
 }
+
+/**
+@typedef {{
+    "serialNodeChanged":
+    {
+        target: NetworkRootNode,
+        oldSerialNode: SerialRootNode | null,
+        newSerialNode: SerialRootNode | null,
+    },
+    "serialNodeConnected":
+    {
+        target: NetworkRootNode,
+        targetSerialNode: SerialRootNode,
+        otherSerialNode: SerialNode | SerialRootNode,
+    },
+    "serialNodeDisconnected":
+    {
+        target: NetworkRootNode,
+        targetSerialNode: SerialRootNode,
+        otherSerialNode: SerialNode | SerialRootNode,
+    },
+}} NetworkRootNodeEventMap
+*/
 
 /**
 */ export class NetworkRootNode
 {
     /**
-    @public */ constructor()
+    @public*/ constructor()
     {
+        /**
+        @type {SerialRootNode | null}
+        @private*/ this._serialNode = null;
 
+        /**
+        @type {CustomEventHandler<{ [K in keyof NetworkRootNodeEventMap]: [event: NetworkRootNodeEventMap[K]] }>}
+        @private*/ this._eventHandler = new CustomEventHandler();
+
+        /**
+        @type {(event: import("./serial-nodes").SerialRootNodeEventMap["nodeConnected"]) => void}
+        @private*/ this._serialOnNodeConnected = (e) =>
+        {
+            this._eventHandler.dispatch("serialNodeConnected", {
+                target: this,
+                targetSerialNode: e.target,
+                otherSerialNode: e.otherNode,
+            });
+        };
+
+        /**
+        @type {(event: import("./serial-nodes").SerialRootNodeEventMap["nodeConnected"]) => void}
+        @private*/ this._serialOnNodeDisconnected = (e) =>
+        {
+            this._eventHandler.dispatch("serialNodeDisconnected", {
+                target: this,
+                targetSerialNode: e.target,
+                otherSerialNode: e.otherNode,
+            });
+        };
+    }
+
+    /**
+    @returns {SerialRootNode | null}
+    @public*/ get serialNode() { return this._serialNode }
+    /**
+    @param {SerialRootNode | null} node
+    @public*/ setSerialNode(node)
+    {
+        if (this._serialNode !== null)
+        {
+            this._serialNode.addEventListener("nodeConnected", this._serialOnNodeConnected);
+            this._serialNode.addEventListener("nodeDisconnected", this._serialOnNodeDisconnected);
+        }
+
+        const oldNode = this._serialNode;
+        this._serialNode = node;
+
+        this._eventHandler.dispatch("serialNodeChanged", {
+            target: this,
+            oldSerialNode: oldNode,
+            newSerialNode: node,
+        });
     }
 }
--- js/serial-nodes.js
+++ js/serial-nodes.js
@@ -1,9 +1,177 @@
+import { CustomEventHandler } from "./custom-events.js";
+import { LoudArray } from "./loud.js";
+
+/**
+@typedef {{
+    "nodeConnected":
+    {
+        target: SerialNode,
+        otherNode: SerialNode | SerialRootNode,
+    },
+    "nodeDisconnected":
+    {
+        target: SerialNode,
+        otherNode: SerialNode | SerialRootNode,
+    },
+}} SerialNodeEventMap
+*/
+
 /**
 */ export class SerialNode
 {
     /**
     @public*/ constructor()
     {
+        /**
+        @type {(SerialNode | SerialRootNode)[]}
+        @private*/ this._connectedTo = [];
 
+        /**
+        @type {CustomEventHandler<{ [K in keyof SerialNodeEventMap]: [event: SerialNodeEventMap[K]] }>}
+        @private*/ this._eventHandler = new CustomEventHandler();
     }
+
+    /**
+    @type {readonly (SerialNode | SerialRootNode)[]}
+    @public*/ get connectedTo() { return this._connectedTo }
+
+    /**
+    @param {SerialNode | SerialRootNode} node
+    @public*/ connectNode(node)
+    {
+        if (this.connectedTo.indexOf(node) > 0)
+            return;
+
+        this._connectedTo.push(node);
+
+        node.connectNode(this);
+
+        this._eventHandler.dispatch("nodeConnected", {
+            otherNode: node,
+            target: this,
+        });
+    }
+
+    /**
+    @param {SerialNode | SerialRootNode} node
+    @public*/ disconnectNode(node)
+    {
+        const index = this.connectedTo.indexOf(node);
+
+        if (index <= 0)
+            return;
+
+        this._connectedTo.splice(index, 1);
+
+        node.disconnectNode(this);
+
+        this._eventHandler.dispatch("nodeDisconnected", {
+            otherNode: node,
+            target: this,
+        });
+    }
+
+    /**
+    @template {keyof SerialNodeEventMap} N
+    @param {N} type
+    @param {(event: SerialNodeEventMap[N]) => void} listener
+    @public*/ addEventListener(type, listener) { this._eventHandler.addListener(type, listener) }
+
+    /**
+    @template {keyof SerialNodeEventMap} N
+    @param {N} type
+    @param {(event: SerialNodeEventMap[N]) => void} listener
+    @public*/ removeEventListener(type, listener) { this._eventHandler.removeListener(type, listener) }
+}
+
+/**
+@typedef {{
+    "nodeConnected":
+    {
+        target: SerialRootNode,
+        otherNode: SerialNode | SerialRootNode,
+    },
+    "nodeDisconnected":
+    {
+        target: SerialRootNode,
+        otherNode: SerialNode | SerialRootNode,
+    },
+}} SerialRootNodeEventMap
+*/
+
+/**
+*/ export class SerialRootNode
+{
+    /**
+    @param {SerialPort} port
+    @public */ constructor(port)
+    {
+        /**
+        @type {LoudArray<SerialNode>}
+        @private*/ this._connectedTo = new LoudArray();
+
+        /**
+        @type {SerialPort}
+        @private*/ this._port = port;
+
+        /**
+        @type {CustomEventHandler<{ [K in keyof SerialRootNodeEventMap]: [event: SerialRootNodeEventMap[K]] }>}
+        @private*/ this._eventHandler = new CustomEventHandler();
+    }
+
+    /**
+    @type {readonly SerialNode[]}
+    @public*/ get connectedTo() { return this._connectedTo.value }
+
+    /**
+    @type {SerialPort}
+    @public*/ get port() { return this._port }
+
+    /**
+    @param {SerialNode} node
+    @public*/ connectNode(node)
+    {
+        if (this.connectedTo.indexOf(node) > 0)
+            return;
+
+        this._connectedTo.push(node);
+
+        node.connectNode(this);
+
+        this._eventHandler.dispatch("nodeConnected", {
+            otherNode: node,
+            target: this,
+        });
+    }
+
+    /**
+    @param {SerialNode} node
+    @public*/ disconnectNode(node)
+    {
+        const index = this.connectedTo.indexOf(node);
+
+        if (index <= 0)
+            return;
+
+        this._connectedTo.splice(index, 1);
+
+        node.disconnectNode(this);
+
+        this._eventHandler.dispatch("nodeDisconnected", {
+            otherNode: node,
+            target: this,
+        });
+    }
+
+    /**
+    @template {keyof SerialRootNodeEventMap} N
+    @param {N} type
+    @param {(event: SerialRootNodeEventMap[N]) => void} listener
+    @public*/ addEventListener(type, listener) { this._eventHandler.addListener(type, listener) }
+
+    /**
+    @template {keyof SerialRootNodeEventMap} N
+    @param {N} type
+    @param {(event: SerialRootNodeEventMap[N]) => void} listener
+    @public*/ removeEventListener(type, listener) { this._eventHandler.removeListener(type, listener) }
 }
