eacd922ff97ab2be4a55bec0ff5104eef9e55c9d
Lumoddy <jaydan@jaysanlunt.com.au>
2025-09-05 16:34:36 +1000

    Imported NBT serialization

---
+++ js/base.js
@@ -0,0 +1,4 @@
+import { NBT } from "./nbt/index.js";
+
+// @ts-ignore allow use in debug console.
+window.NBT = NBT;
---
+++ js/nbt/index.js
@@ -0,0 +1,7 @@
+import * as Types from "./types.js";
+import * as Binary from "./binary.js";
+export const NBT = Object.freeze(
+{
+    ...Types,
+    ...Binary,
+});
